08c48191d8e294e7abb833b7950660fe512f74bb
kirtymeena <kirtymeena@gmail.com>
2026-05-23 13:09:22 +0530

    static folder setup

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const PORT = 8000
 // initialze express
 const app = express()
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
-app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'))
-})
+// setup static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
     console.log(`server is running on ${ PORT }`)
