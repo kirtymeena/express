1013e0542c3e9f3cd895da3a1ebc6161b005ef3d
kirtymeena <kirtymeena@gmail.com>
2026-05-23 13:51:53 +0530

    two end points

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ let posts = [
     { id: 3, title: "Post 3" },
 ]
 
-// get all posts
+// get limited posts
+app.get('/api/posts', (req, res) => {
+    console.log(req.query)
+    const limit = parseInt(req.query.limit)
+    if (!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit))
+    }
+    res.status(400).json({ msg: "Invalid query" })
+
+})
+
+// get single post
 app.get("/api/posts/:id", (req, res) => {
     const id = parseInt(req.params.id)
     const post = posts.find(post => post.id === id)
