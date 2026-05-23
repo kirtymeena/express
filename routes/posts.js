5459b040329fed07054e349849db6dc8be14ac22
kirtymeena <kirtymeena@gmail.com>
2026-05-23 15:18:28 +0530

    Put and DELETE end points

--- routes/posts.js
+++ routes/posts.js
@@ -45,4 +45,29 @@ router.post("/", (req, res) => {
     res.status(201).json(posts)
 })
 
+
+// update post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.find(post => post.id === id)
+    if (!post) {
+        return res.status(404).json({ msg: `Post with ${ id } was not found` })
+    }
+    // updating the title if the requested post id
+    post.title = req.body.title
+    res.status(200).json(post)
+})
+
+
+// Delete post
+router.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.filter(post => post.id !== id)
+    if (!post) {
+        return res.status(404).json({ msg: `Post with Id ${ id } was not found` })
+    }
+    res.status(200).json(post)
+})
+
+
 export default router
